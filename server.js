31daf1dac650e846c4b58d1a833cd33f38543f2a
Vinit Yadav <vinityk2004@gmail.com>
2026-04-01 16:05:51 +0530

    fixed express 5 routing

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use('/api/scan', require('./routes/scan'));
 // Serve Static Assets in Production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'dist')));
-    app.get('/*', (req, res) => {
+    app.get('(.*)', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
     });
 } else {
